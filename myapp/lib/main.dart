38e2f43369cad6585e97aa7e12812afa494ebf83
chaitraboggaram <113207424+chaitraboggaram@users.noreply.github.com>
2024-02-01 17:48:52 -0800

    Stateless Widget with Nico Robin card

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -8,57 +8,114 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text(
-          "My App",
+          "Straw Hat Pirates",
           style: TextStyle(
             color: Colors.white,
             fontFamily: 'Oswald',
           ),
         ),
         centerTitle: true,
-        backgroundColor: Colors.pink[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,     // Removes the drop shadow from the app
       ),
-      body: Row(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.blue,
-              child: Text("1"),
+      
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/robin.jpeg'),
+                radius: 40.0,
+              ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.orange,
-              child: Text("2"),
+            Divider(
+              height: 40.0,
+              color: Colors.grey[800],
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.green,
-              child: Text("3"),
+            Text(
+              "NAME",
+              style: TextStyle(
+                fontFamily: 'Oswald',
+                color: Colors.white,
+                letterSpacing: 2.0,   // Gives 2 pixels of space between each letter
+              ),
             ),
-          ),
-        ],
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text(
-          "Click",
-          style: TextStyle(
-            color: Colors.white,
-            fontFamily: 'Oswald',
-          ),
+            SizedBox(height: 5.0),
+            Text(
+              "Nico Robin",
+              style: TextStyle(
+                fontFamily: 'Oswald',
+                color: Colors.yellow[300],
+                // fontWeight: FontWeight.w200,
+                fontSize: 28,
+                letterSpacing: 2.0,   // Gives 2 pixels of space between each letter
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              "MEMBER NUMBER",
+              style: TextStyle(
+                fontFamily: 'Oswald',
+                color: Colors.white,
+                letterSpacing: 2.0,   // Gives 2 pixels of space between each letter
+              ),
+            ),
+            SizedBox(height: 5.0),
+            Text(
+              "7",
+              style: TextStyle(
+                fontFamily: 'Oswald',
+                color: Colors.yellow[300],
+                // fontWeight: FontWeight.w200,
+                fontSize: 28,
+                letterSpacing: 2.0,   // Gives 2 pixels of space between each letter
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              "ABOUT",
+              style: TextStyle(
+                fontFamily: 'Oswald',
+                color: Colors.white,
+                letterSpacing: 2.0,   // Gives 2 pixels of space between each letter
+              ),
+            ),
+            SizedBox(height: 5.0),
+            Text(
+              "Nico Robin has the ability to sprout duplicate body parts from any surface and control them. This ability is called Hana Hana no Mi, which is a Paramecia-type Devil Fruit. Robin can sprout over a thousand body parts simultaneously, including arms, legs, nets, and wings. She can also use her powers to form giant arms and legs to defeat enemies.",
+              style: TextStyle(
+              fontFamily: 'Oswald',
+              color: Colors.yellow[300],
+              // fontWeight: FontWeight.w200,
+              letterSpacing: 2.0,   // Gives 2 pixels of space between each letter
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 10.0,),
+                Text(
+                  "straw_hat_pirates@onepiece.com",
+                  style: TextStyle(
+                    fontFamily: 'Oswald',
+                    color: Colors.white,
+                    letterSpacing: 1.0,
+                  )
+                ),
+              ],
+            ),
+          ],
         ),
-        backgroundColor: Colors.pink[600],
-      ),
+      )
     );
   }
 }
